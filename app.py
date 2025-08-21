c156a0168c56d977089de9905e7e45860a83df1c
ADMINISTRADOR 1 <jf.castillog@uea.edu.ec>
2025-08-21 05:55:30 -0500

    cambios para tarea 10

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1 style='color:blue;'>¡Bienvenido a mi aplicación Flask!"
+    #return "<h1 style='color:blue;'>¡Bienvenido a mi aplicación Flask!"
+    return render_template('index.html', title='Inicio')
 
 @app.route('/usuarios/<nombre>')
 def ususarios(nombre):
     return f"<h2 style='color:blue;'>¡Hola, {nombre} gracias por visitar mi aplicación!"
+
+@app.route('/about/')
+def about():
+    return render_template('about.html', title='Acerca de')
 
 #if __name__ == '__main__':
 #    app.run(debug=True)
